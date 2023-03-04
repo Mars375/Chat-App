9219f5a6b0fd9c96beb706345e8aa3ec098bffba
Loic Rossi <loic_2610@hotmail.fr>
2023-03-04 19:54:31 +0100

    fetch chats lists of currentUser

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
+import React from "react";
 
 // Import icons
-import Cam from '../Assets/cam.png'
-import Add from '../Assets/add.png'
-import More from '../Assets/more.png'
+import Cam from "../Assets/cam.png";
+import Add from "../Assets/add.png";
+import More from "../Assets/more.png";
 
 // Import components
-import MessageList from './MessageList'
-import Input from './Input'
+import MessageList from "./MessageList";
+import Input from "./Input";
 
 const Chat = () => {
-  return (
-    <div className='chat'>
-      <div className="chatInfo">
-        <span>John</span>
-        <div className="chatIcons">
-          <img src={Cam} alt="" />
-          <img src={Add} alt="" />
-          <img src={More} alt="" />
-        </div>
-      </div>
-      <MessageList/>
-      <Input />
-    </div>
-  )
-}
+	return (
+		<div className="chat">
+			<div className="chatInfo">
+				<span>John</span>
+				<div className="chatIcons">
+					<img src={Cam} alt="" />
+					<img src={Add} alt="" />
+					<img src={More} alt="" />
+				</div>
+			</div>
+			<MessageList />
+			<Input />
+		</div>
+	);
+};
 
-export default Chat
+export default Chat;
--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import { db } from "../firebase";
 
 const ChatList = () => {
-return (
-    <div className='chatList'>
-      <div className="userChat">
-        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-        <div className="userChatInfo">
-          <span>John</span>
-          <p>Hello</p>
-        </div>
-      </div>
-    </div>
-  )
-}
+	const [chats, setChats] = useState([]);
 
-export default ChatList
+	const { currentUser } = useContext(AuthContext);
+
+	useEffect(() => {
+		const getChats = () => {
+			const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+				setChats(doc.data());
+			});
+
+			return () => unsub();
+		};
+
+		currentUser.uid && getChats();
+	}, [currentUser.uid]);
+
+	return (
+		<div className="chatList">
+			{Object.entries(chats)?.map((chat) => (
+				<div className="userChat" key={chat[0]}>
+					<img src={chat[1].userInfo.photoURL} alt="" />
+					<div className="userChatInfo">
+						<span>{chat[1].userInfo.displayName}</span>
+						<p>{chat[1].userInfo.lastMessage?.text}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default ChatList;
